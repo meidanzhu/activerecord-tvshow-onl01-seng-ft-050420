140effe83fb285dd61b4869cbbd5d85f5dd0158b
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-06-10 03:58:08 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -9,6 +9,6 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.where("rating = ?", self.highest_rating)
+    Show.where("rating = #{self.highest_rating}").first
   end
 end
